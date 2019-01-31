b965a6b275b7c23b0f97a433a1f6cd87def95896
Mzwa <mshawnlouw@gmail.com>
2019-01-31 11:40:09 +0200

    9days of CommuniKayTha app with nodeJs n ExpressJs, styling comin soon!

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+    usernames = [];
+
+server.listen(process.env.port || 8080);
+console.log('server on point!!!')
+
+app.get('/', function (req, res) {
+    // when you go to home streen slash load this file
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+io.sockets.on('connection', function(socket) {
+    console.log('Socket connected ...');
+
+// dont want to have double usernames if one taken otherone cannot use that one
+    socket.on('new user', function(data, callback){
+    // CHECK IF THE USERNAME IS THERE
+        if(usernames.indexOf(data) != -1){
+    
+            callback(false)
+        }
+        else{
+            callback(true);
+            socket.username = data;
+        // ADD IT TO USERNAME
+            usernames.push(socket.username);
+            updateUsernames()
+        }
+    })
+
+    // updatie usernames
+    function updateUsernames(){
+// --- Passing the names so that the can be seen on sidebar
+        io.sockets.emit('usernames', usernames)
+    }
+
+    // send message
+    socket.on('send text', function(data) {
+        io.sockets.emit('new message', {
+            msg : data,
+            user: socket.username
+        });
+    })
+// disconnect event!
+    socket.on('disconnect', function(data){
+        // 
+        if(!socket.username){
+            return;
+        }
+// We want to take user out of usernames array then pass second param of 1
+        usernames.splice(usernames.indexOf(socket.username), 1)
+        updateUsernames();
+    })
+})
